d3508b8a713d41443f8a4ff107581e915fb4589d
ifeelgitty <42966018+ifeelgitty@users.noreply.github.com>
2018-09-19 18:12:32 +0200

    Agent changes and ex. Agent
    Changed a lot around the agent-class, creating an example regarding the restaurant. Further developments and explanations will be embedded in the code, as well as functions regarding the class of the agent. So far, everything is implemented in the agent's class and the way it is structured, no second class is needed. This might still change, however right now I'm keeping it this way, as all sets of actions, possible states and connections between the two are unique to the agents anyway, shared actions can later be found out by a class method of the class agents, but this is still a bit fuzzy.

--- aatsvaluationsomethingitreallydoesntmatter.py
+++ aatsvaluationsomethingitreallydoesntmatter.py
@@ -1,6 +1,65 @@
 # -*- coding: utf-8 -*-
 
 class Agent:
-    def __init__(self, curr_state, values):
+    # initializing the agent. The last 4 attributes are empty if not provided
+    # and can be added afterwards as well
+    def __init__(self, 
+                 values, 
+                 curr_state, 
+                 all_states={}, 
+                 all_actions={},
+                 poss_actions={},
+                 sys_transition={}):
+        # values is a dictionary containing the personal values of the agent.
+        self.values = values        
+        # curr_state is the state an agent is in at the moment
         self.curr_state = curr_state
-        self.values = values
+        # all_states are all possible states an agent could be in
+        self.all_states = all_states
+        # all_actions are all possible actions an agent could take
+        self.all_actions = all_actions
+        # poss_actions is a dictionary containing an entry for each action
+        # which lists all states that this action can be taken from.
+        self.poss_actions = poss_actions
+        
+        
+    # Adding a new dictionary of values to the existing values.
+    # Careful: Existing values will be overwritten!
+    def update_values(self, new_values):
+        self.values.update(new_values)
+        
+
+        
+king_orange = Agent(
+        # values
+        {"quality" : 0.9,
+         "low_price" : 0.05,
+         "low_effort" : 0.5},
+        # current state
+        {"hungry":True},
+        # all states
+        [{"hungry":True},
+         {"hungry":False}],
+        # all actions
+        {"homefood":
+             {"quality":0.1,
+              "low_price":0.95,
+              "low_effort":0.9},
+         "mcburgercrap":
+             {"quality":0.2,
+              "low_price":0.85,
+              "low_effort":0.7},
+         "michelin":
+             {"quality":0.8,
+              "low_price":0.1,
+              "low_effort":0.7}},
+        # all possible actions
+        {"homefood" : "hungry",
+         "mcburgercrap" : "hungry",
+         "michelin" : "hungry"}
+        )
+
+class State:
+    def __init__(self):
+        pass
+        
