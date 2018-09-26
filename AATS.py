54dcfb5cd294d5cf1f2e2108be4e342631dfa956
sanneeggengoor <sanneeggengoor@gmail.com>
2018-09-26 15:39:47 +0200

    start formal AATS

---
+++ AATS.py
@@ -0,0 +1,18 @@
+
+# finite, non-empty set of states with the first item as initial state
+Q = [q0,q1,q2]
+# finite, non-empty set of agents
+Ag = [ag1, ag2, ag3]
+# sets of possible actions for every agent where the intersecten of Aci and Acj is empty
+Ac1 = [ac1a, ac1b, ac1c]
+Ac2 = [ac2a, ac2b, ac2c]
+Ac3 = [ac3a, ac3b, ac3c]
+
+
+# will return the set of states from which every action in ActionSet may be executed
+def rho(ActionSet):
+    pass
+
+# will return state that would result from JointAction in state State
+def tau(State, JointAction):
+    pass
