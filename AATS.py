cc0b6b8b4aad27060bbc667fec1463898ded7419
sanneeggengoor <sanneeggengoor@gmail.com>
2018-10-18 19:31:50 +0200

    ooh yeah model 1 is donegit pull!(i think)(i hope)

--- AATS.py
+++ AATS.py
@@ -1,5 +1,6 @@
 '''FORMAL AATS'''
 
+import operator
 
 class State:
     def __init__(self,
@@ -10,10 +11,12 @@ class Action:
     def __init__(self,
                 initial_states,
                 resulting_state,
-                utilities):
+                utilities,
+                name):
         self.initial_states = initial_states
         self.resulting_state = resulting_state
         self.utilities = utilities
+        self.name = name
 
 class Agent:
     def __init__(self,
@@ -27,7 +30,7 @@ class Agent:
 #### STATES
 
 home_before_eating = State(
-        {"hungry": True}            #dictionary with propositions (from Phi)
+        {"hungry": True},            #dictionary with propositions (from Phi)
 )
 after_dinner_michelin = State(
         {"hungry": False}
@@ -46,23 +49,27 @@ Q = [home_before_eating,
 k_goes_to_michelin = Action(
         [home_before_eating],
         after_dinner_michelin,
-        {"quality":0.8, "low_price":0.1}
+        {"quality":0.8, "low_price":0.1},
+        "King orange goes to Michelin"
 )
 k_goes_to_febo = Action(
         [home_before_eating],
         after_dinner_febo,
-        {"quality":0.2, "low_price":0.9}
+        {"quality":0.2, "low_price":0.9},
+        "King orange goes to FEBO"
 )
 
 t_goes_to_michelin = Action(
         [home_before_eating],
         after_dinner_michelin,
-        {"quality":0.8, "low_price":0.1}
+        {"quality":0.8, "low_price":0.1},
+        "Tokkie goes to Michelin"
 )
 t_goes_to_febo = Action(
         [home_before_eating],
         after_dinner_febo,
-        {"quality":0.2, "low_price":0.9}
+        {"quality":0.2, "low_price":0.9},
+        "Tokkie goes to FEBO"
 )
 
 ac = [k_goes_to_michelin, k_goes_to_febo, t_goes_to_michelin, t_goes_to_febo]
@@ -81,6 +88,7 @@ tokkie = Agent(
 # finite, non-empty set of agents
 ag = [king_orange, tokkie]
 
+valueset = ["quality", "low_price"]
 
 # finite, non-empty set of atomic propositions (things like: hungry, home etc., these could both be true or false)
 # Phi = [prop1, prop2, prop3]
@@ -108,12 +116,8 @@ def tau(State, Action):
 
 # will return the set of primitive propositions satisfied in each case
 def pi(State):
-    propositions = State.propositions
-    proplist = []
-    for item in phi:
-        if propositions.get(item):
-            proplist.append(item)
-    return proplist
+    return State.propositions
+
 
 # print(pi(home_before_eating))
 
@@ -121,6 +125,8 @@ def get_involved_agent(Action):
     for agent in ag:
         if Action in agent.actions:
             return agent
+
+
 
 def delta(Action):
     involved_agent = get_involved_agent(Action)
@@ -134,20 +140,57 @@ def delta(Action):
 #stage 1 critical questions
 def problem_formulation(Init_state,Resulting_state, Action, goal):
     involved_agent = get_involved_agent(Action)
+    ## CQ 2
     if tau(Init_state, Action) != Resulting_state:
         return False
-    elif goal not in pi(Init_state):
+    ## CQ 3
+    if not all(x in set(list(pi(Init_state).keys())) for x in set(list(goal.keys()))):
         return False
-    elif Init_state not in Q:
+    ## CQ12
+    if Init_state not in Q:
         return False
-    elif Action not in ac:
+    ## CQ13
+    if Action not in ac:
         return False
-    elif tau(Init_state, Action) not in Q:
+    ## CQ14
+    if tau(Init_state, Action) not in Q:
         return False
-    # elif involved_agent.values
-    else:
-        return True
+    ## CQ 15
+    if not all(x in set(valueset) for x in set(involved_agent.values.keys())):
+        return False
 
+    ## CQ 16
+    for state in Q:
+        if all(x in set(list(pi(state).keys())) for x in  set(list(goal.keys()))):
+            for value in list(goal.keys()):
+                if goal.get(value) != pi(state).get(value):
+                    break
+                return True
+    return False
+
+def epistemic_reasoning(Init_state, Action):
+    ## CQ 1
+    if not Init_state == Q[0]:
+        return False
+    if not Init_state in rho(Action):
+        return False
+    return True
+
+
+
+def choose_action(Init_state, Agent, goal):
+    actions_agent = Agent.actions
+    possible_actions = []
+    for action in actions_agent:
+        res_state = tau(Init_state, action)
+        if problem_formulation(Init_state, res_state, action, goal):
+            if epistemic_reasoning(Init_state, action):
+                possible_actions.append(action)
+    action_value_dict = {}
+    for pos_action in possible_actions:
+        action_value_dict[pos_action.name] = delta(pos_action)
+    print("The possible actions with their values for this agent are:\t \t" + str(action_value_dict))
+    return max(action_value_dict.items(), key=operator.itemgetter(1))[0]
 
 
 
@@ -162,7 +205,14 @@ def problem_formulation(Init_state,Resulting_state, Action, goal):
 # print("Delta functions:")
 # print(Q)
 # print(tau(home_before_eating,k_goes_to_michelin))
-print("Expected utility of Tokkie going to Michelin:\t \t" + str(delta(t_goes_to_michelin)))
-print("Expected utility of Tokkie going to Febo:\t \t" +str(delta(t_goes_to_febo)))
-print("Expected utility of King Orange going to Michelin:\t" +str(delta(k_goes_to_michelin)))
-print("Expected utility of King Orange going to Febo:\t \t" +str(delta(k_goes_to_febo)))
+
+#
+
+# print("Expected utility of Tokkie going to Michelin:\t \t" + str(delta(t_goes_to_michelin)))
+# print("Expected utility of Tokkie going to Febo:\t \t" +str(delta(t_goes_to_febo)))
+# print("Expected utility of King Orange going to Michelin:\t" +str(delta(k_goes_to_michelin)))
+# print("Expected utility of King Orange going to Febo:\t \t" +str(delta(k_goes_to_febo)))
+# print("pi(state home_before_eating): \t \t" + str(pi(home_before_eating)))
+# print("Problem Formulation (home hungry to after_dinner_michelin with king to michelin and goal hungry true):\t \t" + str(problem_formulation(home_before_eating, after_dinner_michelin, k_goes_to_michelin, {"hungry": True})))
+# print("epistemic_reasoning:\t \t" + str(epistemic_reasoning(home_before_eating,k_goes_to_michelin)))
+print("Action that is chosen for agent King Orange who is hungry and home:\t \t" + str(choose_action(home_before_eating, king_orange, {"hungry":False})))
