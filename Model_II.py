78bc3d54da369906543ca6f859fe387d8195856f
ifeelgitty <42966018+ifeelgitty@users.noreply.github.com>
2018-10-18 14:06:42 +0200

    Implemented State-class and valuation of state-changes
    - Implementation of State class, holding current and desired state attributes per state as numbers, setting a threshold that defines from what value on, a change towards the desired value is attempted.
    - The favorability of different actions now depends on a valuation based on how much the respective actions bring the current value closer to the desired value. There are even different state change functions, either setting the new state to a certain value(hunger gets set to 0 after eating), or changing it based on the current value(for example, stress level might be decreased by 0.3, which for a current stress level of 0.9 would lead to a new state of 0.6.).
    - The structure of the whole code has been adjusted to the new structure.
    - Some revisions will still take place based on the example we will choose and to increase readability. This also includes the addition of revisioned comments.

--- Model_II.py
+++ Model_II.py
@@ -7,16 +7,13 @@ class Agent:
     # and can be added afterwards as well
     def __init__(self,
                  values,
-                 curr_state,
-                 desired_state={},
+                 states,
                  all_actions={}):
         # values is a dictionary containing the personal values of the agent.
         self.values = values
         # curr_state is the state an agent is in at the moment. It is a
         # dictionary consisting of attribute-value pairings, so far booleans.
-        self.curr_state = curr_state
-        # State that at this moment is desired
-        self.desired_state = desired_state
+        self.states = states
         """
         All actions are all actions an agent can take. This is a dictionary
         with entries for all actions. These entries at the moment have each a
@@ -36,24 +33,42 @@ class Agent:
     # It also gives output with information of all steps.
     def act(self):
         """ Functions used inside act """
+        def curr_state():
+            current_state_dict = {}
+            current_state_prints = []
+            desired_state = {}
+            for i in self.states:
+                state = self.states[i]
+                if state.current >= state.threshold:
+                    current_state_dict[i] = True
+                    current_state_prints.append([i, state.current])
+                    desired_state[i] = state.desired
+                else:
+                    current_state_dict[i] = False
+            return current_state_dict, current_state_prints, desired_state
+                    
         # For loops through list of possible action-state pairings to
         # see which ones are actable based on current state, returns list.
-        def curr_poss_act():
+        def curr_poss_act(current_state_dict):
             curr_actions = []
             for i in self.all_actions:
                 for j in self.all_actions[i].possible_states:
-                    if j == self.curr_state:
+                    if j == current_state_dict:
                         curr_actions.append(i)
             return curr_actions
         
-        def curr_des_act(actions):
+        def curr_des_act(actions, desired_state):
             curr_actions = {}
             for i in actions:
                 target_meter = 0
-                for j in self.desired_state:
+                for j in desired_state:
                     if j in self.all_actions[i].state_changes:
-                        if self.desired_state[j] == self.all_actions[i].state_changes[j]:
-                            target_meter += 1
+                        action_state_changes = self.all_actions[i].state_changes[j]
+                        change = self.all_actions[i].state_change_function(self.states[j].current, 
+                                                 action_state_changes[0], 
+                                                 action_state_changes[1])
+                        change_factor = self.states[j].current - change / self.states[j].current - desired_state[j]
+                        target_meter += change_factor * self.values[j]
                 if len(curr_actions) > 0:
                     if curr_actions["meter"] == target_meter:
                         curr_actions["actions"].append(i)
@@ -63,7 +78,7 @@ class Agent:
                 else:
                     curr_actions["meter"] = target_meter
                     curr_actions["actions"] = [i]
-            return curr_actions["actions"]
+            return curr_actions["actions"]                        
 
         # Valuation Function to calculate utilities of all poss. actions
         def val_function(curr_actions):
@@ -79,12 +94,13 @@ class Agent:
         """ Beginning of the "actual" function "act" """
 
         # Getting all possible actions at this moment
-        print("Current State: " + str(self.curr_state))
-        curr_actions = curr_poss_act()
+        current_state_dict, current_state_prints, desired_state = curr_state()
+        print("Current State: " + str(current_state_prints))
+        curr_actions = curr_poss_act(current_state_dict)
         print("Possible Actions: " + str(curr_actions))
         
         # Calculating desired state actions
-        curr_actions = curr_des_act(curr_actions)
+        curr_actions = curr_des_act(curr_actions, desired_state)
         print("Actions with most desired states: " + str(curr_actions))
 
         # Calculating valuation
@@ -100,24 +116,44 @@ class Agent:
         # Updating current state
         ca_statchange = self.all_actions[chosen_action[1]].state_changes
         for i in ca_statchange:
-            self.curr_state[i] = ca_statchange[i]
-        print("New State: " + str(self.curr_state))
+            changes = ca_statchange[i]
+            self.states[i].current = self.all_actions[chosen_action[1]].state_change_function(self.states[i].current,
+                       changes[0], changes[1])
+        current_state_dict, current_state_prints, desired_state = curr_state()
+        print("New State: " + str(current_state_prints))
 
 class Action:
     def __init__(self,
-                 state_changes = {}, 
+                 state_changes = {},
                  attributes = {}, 
                  possible_states = []):
         self.state_changes = state_changes
         self.attributes = attributes
         self.possible_states = possible_states
+    def state_change_function(self, state, change, fun):
+        if fun == "set":
+            return change
+        if fun == "add":
+            return state + change
+        if fun == "mul":
+            return state * change
+        
+# Empty class for State, so far placeholder
+class State:
+    def __init__(self,
+                 current = None,
+                 desired = None,
+                 threshold = 0.5):
+        self.current = current
+        self.desired = desired
+        self.threshold = threshold
         
         
 # King Orange's actions
 orange_actions = {}
 # Homefood
 orange_actions["homefood"] = Action()
-orange_actions["homefood"].state_changes = {"hungry" : False}
+orange_actions["homefood"].state_changes = {"hungry" : [0, "set"]}
 orange_actions["homefood"].attributes = {"quality":0.1,
                                          "low_price":0.95,
                                          "low_effort":0.9}
@@ -125,7 +161,7 @@ orange_actions["homefood"].possible_states = [{"hungry":True}]
 
 # McBurgercrap
 orange_actions["mcburgercrap"] = Action()
-orange_actions["mcburgercrap"].state_changes = {"hungry" : False}
+orange_actions["mcburgercrap"].state_changes = {"hungry" : [0, "set"]}
 orange_actions["mcburgercrap"].attributes = {"quality":0.2,
                                          "low_price":0.85,
                                          "low_effort":0.7}
@@ -133,32 +169,37 @@ orange_actions["mcburgercrap"].possible_states = [{"hungry":True}]
 
 # Michelin
 orange_actions["michelin"] = Action()
-orange_actions["michelin"].state_changes = {"hungry" : False}
+orange_actions["michelin"].state_changes = {"hungry" : [0, "set"]}
 orange_actions["michelin"].attributes = {"quality":0.8,
                                          "low_price":0.1,
                                          "low_effort":0.7}
 orange_actions["michelin"].possible_states = [{"hungry":True}]
 
-    
+# King Orange's States
+orange_states = {}
+# Hungry
+orange_states["hungry"] = State()
+orange_states["hungry"].current = 1
+orange_states["hungry"].desired = 0
+orange_states["hungry"].threshold = 0.6
+
+
 
 # Start of example Agent
 king_orange = Agent(
         # values
         {"quality" : 0.9,
          "low_price" : 0.05,
-         "low_effort" : 0.5},
+         "low_effort" : 0.5,
+         "hungry" : 0.8},
         # current state
-        {"hungry" : True},
-        # desired state
-        {"hungry" : False},
+        orange_states,
         # all actions
         orange_actions
         )
 
-# Empty class for State, so far placeholder
-class State:
-    def __init__(self):
-        pass
+
+        
 
 # Using act function of example Agent to see what happens
 king_orange.act()
