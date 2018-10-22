8ba7687504d98b96a0be229a3aceb9a6827f975e
ifeelgitty <42966018+ifeelgitty@users.noreply.github.com>
2018-10-22 16:32:31 +0200

    Completely revised documentation of Model II
    (Even though no one cares about it or will read it. ever.)

--- Model_II.py
+++ Model_II.py
@@ -3,16 +3,19 @@
 
 """
 class Agent:
-    # initializing the agent. The last 2 attributes are empty if not provided
-    # and can be added afterwards as well
+    # initializing the agent, including its values, states and actions
     def __init__(self,
                  values,
                  states,
                  all_actions={}):
         # values is a dictionary containing the personal values of the agent.
         self.values = values
-        # curr_state is the state an agent is in at the moment. It is a
-        # dictionary consisting of attribute-value pairings, so far booleans.
+        """
+        states is a dictionary containing elements of the "state"-class. Each
+        element is an element of the state with a "current" and a "desired"
+        state. The "current" state of the agent can be described as the sum of
+        the "current" values of all elements of the state-class.
+        """
         self.states = states
         """
         All actions are all actions an agent can take. This is a dictionary
@@ -32,23 +35,40 @@ class Agent:
     # Making the agent act based on current state, including change in state.
     # It also gives output with information of all steps.
     def act(self):
+        
         """ Functions used inside act """
+        
+        """
+        Returning the "current state" of the agent as well as the "desired
+        state" by looping through all state-items belonging to the agent. 
+        Whether a state-item will be part of the desired state depends on
+        whether the current state is above a certain threshold. This threshold
+        defines the point at which the agent feels like the current state is
+        so unsatisfying that a change towards the desired state is necessary.
+        """
         def curr_state():
-            current_state_dict = {}
-            current_state_prints = []
-            desired_state = {}
+            current_state_dict = {} # Contains all attributes of current state
+            current_state_prints = [] # Contains info to print curr_state
+            desired_state = {} # Contains all attributes of desired state
+            # Looping through all state-elements
             for i in self.states:
                 state = self.states[i]
+                # Checking whether element is above threshold
                 if state.current >= state.threshold:
+                    # State of element set to "True", as its above threshold
                     current_state_dict[i] = True
                     current_state_prints.append([i, state.current])
+                    # Element added to desired state
                     desired_state[i] = state.desired
                 else:
                     current_state_dict[i] = False
+                    current_state_prints.append([i, state.current])
             return current_state_dict, current_state_prints, desired_state
                     
-        # For loops through list of possible action-state pairings to
-        # see which ones are actable based on current state, returns list.
+        """
+        Purely testing what actions are possible from the current state, 
+        regardless of whether these are at all desirable for the agent.
+        """
         def curr_poss_act(current_state_dict):
             curr_actions = []
             for i in self.all_actions:
@@ -57,11 +77,28 @@ class Agent:
                         curr_actions.append(i)
             return curr_actions
         
+        """
+        Taking possible actions and desired state, computing which actions
+        lead to the most desirable new state. All actions that satisfy this
+        requirement are returned.
+        """
         def curr_des_act(actions, desired_state):
-            curr_actions = {}
+            curr_actions = {} # Holding the actions' names and "meters"
+            # Looping through all possible actions
             for i in actions:
-                target_meter = 0
+                target_meter = 0 # Tracks "desiredness" for each action
+                # Looping through state-elements of desired state
                 for j in desired_state:
+                    """
+                    For each element of the desired state, it is checked
+                    whether a change in this direction is represented in the
+                    action. The closer the change brings the new state to the
+                    desired state, the higher this will be valued. Ultimately,
+                    this is represented in the target meter. This 
+                    desiredness-value is also multiplied by the factor in the
+                    value system of the agent, as changes in different states
+                    might be differently important to the agent.
+                    """
                     if j in self.all_actions[i].state_changes:
                         action_state_changes = self.all_actions[i].state_changes[j]
                         change = self.all_actions[i].state_change_function(self.states[j].current, 
@@ -80,7 +117,12 @@ class Agent:
                     curr_actions["actions"] = [i]
             return curr_actions["actions"]                        
 
-        # Valuation Function to calculate utilities of all poss. actions
+        """
+        Of all "selected actions" leading to equally desirable new states, the 
+        utilities regarding the "secondary properties" is done and returned in
+        a sorted manner. This is about "how" the new state is reached, not
+        how desirable the state itself is.
+        """
         def val_function(curr_actions):
             utility_list = []
             for i in curr_actions:
@@ -93,28 +135,34 @@ class Agent:
 
         """ Beginning of the "actual" function "act" """
 
-        # Getting all possible actions at this moment
+        # Getting current state, printing it
         current_state_dict, current_state_prints, desired_state = curr_state()
         print("Current State: " + str(current_state_prints))
+        
+        # Calculating all possible actions from the current state
         curr_actions = curr_poss_act(current_state_dict)
         print("Possible Actions: " + str(curr_actions))
         
-        # Calculating desired state actions
+        # Calculating actions that lead to new state closest to desired state
         curr_actions = curr_des_act(curr_actions, desired_state)
         print("Actions with most desired states: " + str(curr_actions))
 
-        # Calculating valuation
+        # Calculating utilities of those actions
         curr_utilities = val_function(curr_actions)
         print("Utilities of actions: ")
         for i in curr_utilities:
             print(str(i[1]) + ": " + str(i[0]))
 
-        # Showing best action
+        # Showing best action!
         chosen_action = curr_utilities[0]
         print("Chosen action by agent: " + chosen_action[1])
 
         # Updating current state
         ca_statchange = self.all_actions[chosen_action[1]].state_changes
+        """
+        Looping through all changes in the current state. The change also
+        depends on which function is used to change the state(see below)
+        """
         for i in ca_statchange:
             changes = ca_statchange[i]
             self.states[i].current = self.all_actions[chosen_action[1]].state_change_function(self.states[i].current,
@@ -122,30 +170,72 @@ class Agent:
         current_state_dict, current_state_prints, desired_state = curr_state()
         print("New State: " + str(current_state_prints))
 
+"""
+The Action-class is a class of all actions that an agent can in principle take.
+"""
 class Action:
     def __init__(self,
                  state_changes = {},
                  attributes = {}, 
                  possible_states = []):
+        """
+        State changes contains a dictionary with all changes that happen when
+        the action is acted out. Each changed state-attribute is represented
+        by its name and a list with both the numeric value for the change and
+        the name of the change function as a string(see below).
+        This looks like:
+            {"name_of_state" : [float_of_change, "name_of_change_function"]}
+        """
         self.state_changes = state_changes
+        """
+        attributes contains a dictionary with all "secondary" attributes used
+        for the computation of the utilities.
+        """
         self.attributes = attributes
+        """
+        A list with dictionaries containing different states the action can
+        be performed from.
+        """
         self.possible_states = possible_states
+    
+    """
+    The state change function returns the new value for the state. Its
+    parameters are "state", which is the value of the current state, "change"
+    which is the change-value and "fun", which is a string that implies which
+    of the functions for calculating the new state is used.
+    New functions can easily be added to this function.
+    """
     def state_change_function(self, state, change, fun):
+        # "Set" simply sets the new state to the value implied by the change.
+        # The current state has no influence on the new state.
         if fun == "set":
             return change
+        # "Add" adds the change to the current state(will mostly be used with
+        # negative change-values to "subtract")
         if fun == "add":
             return state + change
+        # "Mul" multiplies the current state by a change-factor.
         if fun == "mul":
             return state * change
         
-# Empty class for State, so far placeholder
+"""
+THe state-class is used o hold information for each state-item.
+"""
 class State:
     def __init__(self,
                  current = None,
                  desired = None,
                  threshold = 0.5):
+        # The current state-value the agent is in
         self.current = current
+        # THe desired state-value the agent would like to be in.
         self.desired = desired
+        """
+        The threshold defines the value from which on the agent feels like a
+        change towards the desired state is needed, ultimately it is being used
+        to convert the "current" state from a float-value to a boolean state
+        that is either true or false.
+        """
         self.threshold = threshold
         
         
